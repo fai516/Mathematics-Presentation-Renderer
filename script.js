379ebcf736e20456ca2b2c9413db7b18ca84128a
fai516 <yfsiuv@gmail.com>
2018-11-17 19:54:36 +0800

    adjust svg textarea height

--- script.js
+++ script.js
@@ -26,7 +26,7 @@ function pickExample(n){
     switch(n){
         case 1: return "f(x|\\mu,\\sigma^2)=\\frac{1}{\\sqrt{2\\pi\\sigma^2}}e^{-\\frac{(x-\\mu)^2}{2\\mu^2}}";
         case 2: return "e^{i\\pi}+1=0";
-        case 3: return "e^{x}=\\lim_{n\\to\\infty}=\\left(1+\\frac{x}{n}\\right)^{n}";
+        case 3: return "e^{x}=\\lim_{n\\to\\infty}\\left(1+\\frac{x}{n}\\right)^{n}";
         case 4: return "e^{ix}=\\cos{x}+i\\sin{x}";
         case 5: return "\\Gamma(z) = \\int_{0}^{\\infty}x^{z-1}e^{-x}dx";
         case 6: return "\\int_{-\\infty}^{\\infty}e^{-x^2}dx=\\sqrt{\\pi}"
@@ -61,20 +61,35 @@ function copySVGCodetoClipboard(timer){
 
 function svgToPng(){
     var svgText = getSvg();
-    var canvas = document.getElementById("canvas");
-    var ctx = canvas.getContext("2d");
-    var DOMURL = self||self.url||self.webkitURL;
-    
-    var img = new Image();
-    var svg = new Blob([svgText], {type: 'image/svg+xml'});
-    var url = (window.DOMURL ? DOMURL : webkitURL).createObjectURL(svg);
+    if(svgText==undefined){alert("SVG is absent or invaild! Check your LATEX Code."); return;}
+    else{
+        var canvas = document.getElementById("canvas");
+        var ctx = canvas.getContext("2d");
+        var DOMURL = self||self.url||self.webkitURL;
+        
+        var img = new Image();
+        var svg = new Blob([svgText], {type: 'image/svg+xml'});
+        var url = (window.DOMURL ? DOMURL : URL).createObjectURL(svg);
+        var png_img = "a";
+        img.onload = function () { //onload event
+            ctx.drawImage(img, 0, 0);
+            (window.DOMURL ? DOMURL : URL).revokeObjectURL(url);
+            png_img = canvas.toDataURL("image/png");
+            downloadFile(png_img);
+            ctx.clearRect(0, 0, canvas.width, canvas.height);//clear drawing or it will get overlap by multiple requests
+        }
+          
+        img.src = url; //this is to trigger onload event
+    }
+}
 
-    img.onload = function () {
-        ctx.drawImage(img, 0, 0);
-        DOMURL.revokeObjectURL(url);
-        var png_img = canvas.toDataURL("image/png");
-      }
-      
-    img.src = url;
-    console.log(url);
+function downloadFile(src){
+    console.log(src);
+    var elem = document.createElement("a");
+    elem.style.display = "none";
+    elem.setAttribute("href",src);
+    elem.setAttribute("download","math.png");
+    document.body.appendChild(elem);
+    elem.click();
+    document.body.removeChild(elem);
 }
